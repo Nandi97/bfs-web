e21a0cb4fb517ef1fc0ec1f3203cbdbd5bbef613
Alvin Kigen <alvinkigen+github@outlook.com>
2026-04-23 22:53:44 -0400

    Update README, add authentication setup, and configure environment variables

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,4 +1,5 @@
 import "dotenv/config";
+import { expo } from "@better-auth/expo";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
@@ -7,6 +8,11 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  trustedOrigins: ["mobile://"],
+  plugins: [expo()],
   secret: process.env.BETTER_AUTH_SECRET,
   baseURL: process.env.BETTER_AUTH_URL,
 });
